975ff9c0d156f670bc67af10e1960bee73e02574
gbarberan23 <gabrielbv.2000@gmail.com>
2023-10-30 23:06:38 -0500

    Mongodb coneccion

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+const app = express()
+dotenv.config()
+
+const connect = async ()=>{
+try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Conectado a mongo")
+  } catch (error) {
+    throw error 
+  }
+};
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("mongo esta desconectado ")
+})
+mongoose.connection.on("connected", ()=>{
+    console.log("mongo connected ")
+})
+
+app.listen(8800, ()=>{
+    connect()
+    console.log("conectado al backend!")
+})
+
+//mongodb+srv://gabriel:<password>@cluster1.xliyhys.mongodb.net/?retryWrites=true&w=majority
