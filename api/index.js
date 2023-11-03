1b0d778c2cc417e48bbf3fba0efab26ede674a33
gbarberan23 <gabrielbv.2000@gmail.com>
2023-11-03 16:01:49 -0500

    controladores

--- api/index.js
+++ api/index.js
@@ -23,12 +23,23 @@ mongoose.connection.on("disconnected", ()=>{
 
 //middelwares
 app.use(express.json())
+
+
 app.use("/api/auth", authRoute);
-app.use("/api/users", usersRoute)
-app.use("/api/hotels", hotelsRoute)
-app.use("/api/rooms", roomsRoute)
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
 
-
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 /*mongoose.connection.on("connected", ()=>{
     console.log("mongo connected ")
