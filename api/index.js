c8a83f76abebf244c4c3c0509a17cc51a5ed69bf
gbarberan23 <gabrielbv.2000@gmail.com>
2023-11-03 17:09:14 -0500

    token

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import authRoute from "./routes/auth.js"
 import usersRoute from "./routes/users.js"
 import hotelsRoute from "./routes/hotels.js"
 import roomsRoute from "./routes/rooms.js"
+import cookieParser from "cookie-parser"
 const app = express()
 dotenv.config()
 
@@ -22,6 +23,7 @@ mongoose.connection.on("disconnected", ()=>{
 })
 
 //middelwares
+app.use(cookieParser())
 app.use(express.json())
 
 
