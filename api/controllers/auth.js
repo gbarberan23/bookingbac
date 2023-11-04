9e5884e4d517d86266450d02c01310d2256eb940
gbarberan23 <gabrielbv.2000@gmail.com>
2023-11-04 01:05:05 -0500

    definir admin y token

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -35,18 +35,16 @@ export const login = async (req, res, next) => {
             return next(createError(400,"Wrong password o username"));
 
         const token = jwt.sign(
-            {id:user._id, isAdmin:user.isAdmin},
-            process.env.JWT)
+          {id: user._id, isAdmin: user.isAdmin},
+            process.env.JWT
+            );
 
-        const {
-            password, isAdmin, ...otherDetails
-        } = user._doc
-        res
-        .cookie("acces_token", token, {
+        const {password, isAdmin, ...otherDetails} = user._doc;
+        res.cookie("access_token", token, {
             httpOnly: true,
         })
         .status(200)
-        .json(otherDetails);
+        .json({...otherDetails});
       } catch (err) {
         next(err);
       }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,32 @@
 import express from "express"
+import { deleteUser, getUser, getUsers, updateUser } from "../controllers/user.js";
+import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+router.get("/checkauthentication" , verifyToken, (req,res,next)=>{
+    res.send("Hello user , you are logged")
+})
+
+router.get("/checkuser/:id" ,verifyUser , (req,res,next)=>{
+    res.send("Hello user , you are logged and you can delete your account")
+})
+
+router.get("/checkadmin/:id" ,verifyAdmin , (req,res,next)=>{
+    res.send("Hello admin , you are logged and you can delete all accounts")
+})
+  
+//UPDATE
+router.put("/:id", updateUser);
+
+//DELETE
+router.delete("/:id",  deleteUser);
+
+//GET
+router.get("/:id", getUser);
+
+//GET ALL
+router.get("/",  getUsers);
 
 
 
